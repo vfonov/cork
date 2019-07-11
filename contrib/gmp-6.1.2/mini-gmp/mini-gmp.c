931a32273891e546df93e73f631549403728cb9c
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2019-07-11 15:55:50 -0400

    Made it work with GCC 7.4.0

--- contrib/gmp-6.1.2/mini-gmp/mini-gmp.c
+++ contrib/gmp-6.1.2/mini-gmp/mini-gmp.c
@@ -50,11 +50,11 @@ see https://www.gnu.org/licenses/.  */
 
 #include "mini-gmp.h"
 
-
+
 /* Macros */
 #ifndef GMP_LIMB_BITS
 #define GMP_LIMB_BITS (sizeof(mp_limb_t) * CHAR_BIT)
-#endif 
+#endif
 
 #define GMP_LIMB_MAX (~ (mp_limb_t) 0)
 #define GMP_LIMB_HIGHBIT ((mp_limb_t) 1 << (GMP_LIMB_BITS - 1))
